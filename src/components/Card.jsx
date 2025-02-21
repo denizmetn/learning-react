a38c5eb0996f401e439e908ec404d500c9daa204
denizmetn <dniizmetiiin@gmail.com>
2025-02-21 20:48:25 +0300

    Card comp
    expenses ve card componentleri oluşturuldu.

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import './Card.css';
+function Card (props){
+    console.log(props.children);
+    const classes = 'card ' + props.className;
+
+    return <div className={classes}>{props.children}</div>
+}
+
+export default Card;
